5b1349e75e054ea8a7e344d6283fd8ed7b2b0211
Nataliarm11 <nrequenam1@gmail.com>
2023-05-04 19:24:40 -0500

    version2, ejercicios faltantes

--- main-.js
+++ main-.js
@@ -1,103 +1,244 @@
 // Primer ejercicio
-let miNombre ="Natalia"
+// let miNombre ="Natalia"
 
 
 // Segundo ejercicio
-let miApellido ="Requena"
+// let miApellido ="Requena"
 
 
 // Tercer ejercicio
-let miEdad = 25
+// let miEdad = 25
 
 
 // Cuarto ejercicio
-let miMascota = "Canela"
+// let miMascota = "Canela"
 
 
 // Quinto ejercicio
-let edadMascota = 12
+// let edadMascota = 12
 
 
 // Sexto ejercicio
-console.log (miNombre)
-console.log (miApellido)
-console.log(miEdad)
-console.log (miMascota)
-console.log (edadMascota)
+// console.log (miNombre)
+// console.log (miApellido)
+// console.log(miEdad)
+// console.log (miMascota)
+// console.log (edadMascota)
 
 // Septimo ejercicio
-const nombreCompleto = miNombre + " " + miApellido
-console.log (nombreCompleto)
+// const nombreCompleto = miNombre + " " + miApellido
+// console.log (nombreCompleto)
 
 // Octavo ejercicio
-let textoPresentacion = ("Mi nombre completo es" + " " + miNombre + " " + miApellido + " " + "tengo" + " " + miEdad + " " + "años" + " y una perrita llamada" + " " + miMascota + " " + "que tiene" + " "+ edadMascota + " " +"años")
+// let textoPresentacion = ("Mi nombre completo es" + " " + miNombre + " " + miApellido + " " + "tengo" + " " + miEdad + " " + "años" + " y una perrita llamada" + " " + miMascota + " " + "que tiene" + " "+ edadMascota + " " +"años")
 
-console.log (textoPresentacion)
+// console.log (textoPresentacion)
 
 
 // Noveno ejercicio
 // Suma
 
-function sumar (numA, numB) {
+// function sumar (numA, numB) {
 
-    console.log (numA);
-    console.log (numB);
+//     console.log (numA);
+//     console.log (numB);
 
-    let resultado = numA + numB
+//     let resultado = numA + numB
 
-    return resultado
-}
+//     return resultado
+// }
 
-let resultadoSumaEdades = sumar (miEdad, edadMascota)
-console.log (resultadoSumaEdades)
+// let resultadoSumaEdades = sumar (miEdad, edadMascota)
+// console.log (resultadoSumaEdades)
 
 // Resta
 
-function restar (numA, numB) {
+// function restar (numA, numB) {
 
-    console.log (numA);
-    console.log (numB);
+//     console.log (numA);
+//     console.log (numB);
 
-    let resultado = numA - numB
+//     let resultado = numA - numB
 
-    return resultado
-}
+//     return resultado
+// }
 
 
-let resultadoRestaEdades = restar (miEdad, edadMascota)
-console.log (resultadoRestaEdades)
+// let resultadoRestaEdades = restar (miEdad, edadMascota)
+// console.log (resultadoRestaEdades)
 
 // Producto edades
 
-function producto (numA, numB) {
+// function producto (numA, numB) {
 
-    console.log (numA);
-    console.log (numB);
+//     console.log (numA);
+//     console.log (numB);
 
-    let resultado = numA * numB
+//     let resultado = numA * numB
 
-    return resultado
-}
+//     return resultado
+// }
 
-let resultadoProductoEdades = producto (miEdad, edadMascota)
-console.log (resultadoProductoEdades)
+// let resultadoProductoEdades = producto (miEdad, edadMascota)
+// console.log (resultadoProductoEdades)
 
 // Division
-function division (numA, numB) {
+// function division (numA, numB) {
 
-    console.log (numA);
-    console.log (numB);
+//     console.log (numA);
+//     console.log (numB);
 
-    let resultado = numA / numB
+//     let resultado = numA / numB
 
-    return resultado
-}
+//     return resultado
+// }
 
-let resultadoDivisionEdades = division (miEdad, edadMascota)
-console.log (resultadoDivisionEdades)
+// let resultadoDivisionEdades = division (miEdad, edadMascota)
+// console.log (resultadoDivisionEdades)
 
 
 // Decimo ejercicio
 
-let textoPresentacionDos = ("Mi nombre completo es" + " " + miNombre + " " + miApellido + " " + "tengo" + " " + miEdad + " " + "años" + " y una perrita llamada" + " " + miMascota + " " + "que tiene" + " "+ edadMascota + " " +"años" + " " + "sumando nuestras edades da" + " " + resultadoSumaEdades + " " + "años," + " " + "restando nuestras edades da" + " " + resultadoRestaEdades + " " + "años," + " " + "multiplicando nuestras edades da" + " " + resultadoProductoEdades + " " + "años," + " " + " y dividiendo nuestras edades da" + " " + resultadoDivisionEdades + " " + "años,")
-console.log (textoPresentacionDos)
+// let textoPresentacionDos = ("Mi nombre completo es" + " " + miNombre + " " + miApellido + " " + "tengo" + " " + miEdad + " " + "años" + " y una perrita llamada" + " " + miMascota + " " + "que tiene" + " "+ edadMascota + " " +"años" + " " + "sumando nuestras edades da" + " " + resultadoSumaEdades + " " + "años," + " " + "restando nuestras edades da" + " " + resultadoRestaEdades + " " + "años," + " " + "multiplicando nuestras edades da" + " " + resultadoProductoEdades + " " + "años," + " " + " y dividiendo nuestras edades da" + " " + resultadoDivisionEdades + " " + "años,")
+// console.log (textoPresentacionDos)
+
+// Once ejercicio
+
+// const alumno = { 
+//     nombre: "Natalia",
+//     apellido: "Requena",
+//     edad: 25,
+//     estatura: 1.65,
+//     signo: "sagitario",
+// }
+
+//  console.log (alumno.nombre)
+//  console.log (alumno.apellido)
+//  console.log (alumno.edad)
+//  console.log (alumno.estatura)
+//  console.log (alumno.signo)
+
+//  console.table (alumno)
+
+
+// Doce ejercicio
+
+// const mascota = {
+//     nombre: "Canela",
+//     apodo: "Tunix",
+//     edad: 12,
+//     color: "beige",
+//     juguetePreferido: "Peluche gato blanco",
+
+// }
+//  console.log (mascota.nombre)
+//  console.log (mascota.apodo)
+//  console.log (mascota.edad)
+//  console.log (mascota.color)
+//  console.log (mascota.juguetePreferido)
+
+//  console.table (mascota)
+
+
+//  Trece ejercicio
+
+// const frutas = [ "Sandia", "Cerezas", "Fresa", "Banano", "Manzana"]
+
+// console.log (frutas)
+
+// console.log (frutas [0])
+// console.log (frutas [1])
+// console.log (frutas [2])
+// console.log (frutas [3])
+// console.log (frutas [4])
+
+
+// Catorce ejercicio
+
+// const numeros = [ "1", "2", "3", "4", "5"]
+
+// console.log (numeros)
+
+// console.log (numeros [0])
+// console.log (numeros [1])
+// console.log (numeros [2])
+// console.log (numeros [3])
+// console.log (numeros [4])
+
+// Quince ejercicio
+
+// const familia = [ "Mamá", "Papá", "Hija", "Hijo", "Mascota"]
+
+// console.log (familia)
+
+// console.log (familia [0])
+// console.log (familia [1])
+// console.log (familia [2])
+// console.log (familia [3])
+// console.log (familia [4])
+
+// // Dieciseis ejercicio
+
+// console.log (frutas[1])
+// console.log (numeros [3])
+// console.log (familia [4])
+
+// let textoAleatorio = ("Un día fui a comprar" + " " + frutas[1] + " " + "compre" + " " + numeros[3] + " " + "de estas y me acompaño mi" + " " + familia[4])
+
+// console.log (textoAleatorio)
+
+// Diecisiete ejercicio
+
+//  let miEdad= Number (prompt ("Ingrese su edad"))
+//  let edadCompañero = Number (prompt ("Ingrese la edad de su compañero"))
+
+// console.log  (` La edad mia es: ${miEdad} años` )
+// console.log  (` La edad de mi compañero es: ${edadCompañero} años` )
+
+// let edadIgual = miEdad === edadCompañero;
+// let mayor = miEdad > edadCompañero;
+// let menor = miEdad < edadCompañero;
+
+// console.log("Mi edad es igual a la de mi compañero:", edadIgual);
+// console.log("Mi edad es mayor a la de mi compañero:", mayor);
+// console.log("Mi edad es menor a la de mi compañero:", menor);
+
+
+
+// Dieciocho ejercicio Compare su edad ingresada por pantalla con prompt() con el numero 18 y guardarlo en una variable llamada soyMayorDeEdad y mostrar por consola un mensaje  que diga: Soy mayor de edad y el valor de la variable.
+
+// let miEdad = Number (prompt ("Ingrese su edad"))
+// console.log (` La edad mia es: ${miEdad} años` )
+
+
+// let soyMayorDeEdad = miEdad >=18; 
+
+// console.log("Soy mayor de edad y el valor de la variable es", soyMayorDeEdad)
+
+
+
+
+
+
+// Diecinueve ejercicio Introducir por pantalla la edad y la altura de una persona y guardarlas en variables  separadas y en una variable llamada puedeSubir el resultado de la operación resultante  de si la persona es mayor de 6 años y además tiene una altura mínima de 120cm y  mostrar por consola un mensaje como el siguiente: Puede subir a la atracción y el valor de  la variable resultante.
+
+// let edad = Number (prompt ("Ingrese su edad"))
+// let altura = Number ( prompt ("Ingrese su altura"))
+
+// let puedeSubir = edad > 6 && altura >= 120;
+
+// console.log ( "puede subir a la atracción:"+ " " + puedeSubir)
+
+// veinte ejercicio Introducir por pantalla el pase de una persona el cual puede ser “VIP”, “NORMAL” o  “LIMITADO”, el saldo que dispone y guardarlos en variables separadas. En una variable  llamada puedePasar guardar el resultado de la operación resultante de si la persona tiene  pase “VIP” o si posee un saldo mayor a 1000. Mostrando un mensaje que diga: La persona puede pasar y el resultado de la variable
+
+// let tipoDePase = prompt("Ingresa el tipo de pase (VIP, NORMAL o LIMITADO):");
+// let saldoDisponible = parseFloat(prompt("Ingresa tu saldo disponible:"));
+
+// // Verificar si la persona puede pasar
+// let puedePasar = tipoDePase === "VIP" || saldoDisponible > 1000;
+
+// // Mostrar el resultado en la consola
+// console.log("La persona puede pasar: " + puedePasar);
+
+
+// console.log ("fin del script")
